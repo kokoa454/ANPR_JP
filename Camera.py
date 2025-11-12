db181001008db77f20ff5501bd0b5c1e99a4aa49
Inari Goro <qqnq7um9k@gmail.com>
2025-11-12 09:31:02 +0900

    fix: update camera logic

--- Camera.py
+++ Camera.py
@@ -2,21 +2,23 @@ __package__ = "Camera"
 
 import cv2
 
+CAMERA_ID = 0
+
 class Camera:
     def __init__(self):
         pass
 
-    def _openCamera(self, camera_id: int):
-        capture = cv2.VideoCapture(camera_id)
+    def _openCamera(self):
+        capture = cv2.VideoCapture(CAMERA_ID)
         return capture
 
     def _closeCamera(self, capture):
         capture.release()
         cv2.destroyAllWindows()
 
-    def captureImage(self, camera_id: int):
+    def captureImage(self):
         try:
-            capture = self._openCamera(camera_id)
+            capture = self._openCamera()
             ret, frame = capture.read()
             self._closeCamera(capture)
             if ret:
