de1eca9a4b8510ea33516072e9de79c1e6135a93
InariGoro <qqnq7um9k@gmail.com>
2026-01-14 11:04:31 +0900

    refactor: update rpicam-jpeg to rpicam-still

--- anpr/entrance/models/camera.py
+++ anpr/entrance/models/camera.py
@@ -16,7 +16,7 @@ class Camera:
     def capture_image(self) -> Image.Image | None:
         try:
             file_name = f"{config.OUTPUT_CAPTURE_DIR}/{Utilities.get_timestamp()}.jpeg"
-            os.system(f"rpicam-jpeg --metering {config.RPICAM_METERING} -n --autofocus-mode {config.RPICAM_AUTOFOCUS_MODE} --output {file_name} --timeout {config.RPICAM_TIMEOUT}")
+            os.system(f"rpicam-still --hdr --zsl --metering {config.RPICAM_METERING} -n --autofocus-mode {config.RPICAM_AUTOFOCUS_MODE} --output {file_name} --timeout {config.RPICAM_TIMEOUT}")
             return Image.open(file_name)
         except Exception as e:
             ErrorLog.save_error_log(timestamp = Utilities.get_timestamp(), error_type = "Camera", error = f"{e}")
