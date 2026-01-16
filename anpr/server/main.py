776a799b821dbcb669fc4419213db5bac6f50a4b
Inari Goro <qqnq7um9k@gmail.com>
2026-01-16 10:11:33 +0900

    feat: define working day checker

--- anpr/server/main.py
+++ anpr/server/main.py
@@ -12,6 +12,11 @@ from fastapi.responses import StreamingResponse
 import csv
 import pandas as pd
 from datetime import date
+from data_models.waiting_time import WaitingTime
+from data_models.attraction import Attraction
+import requests
+from bs4 import BeautifulSoup
+import re
 
 # 環境変数確認
 if config.DATABASE_URL is None:
@@ -68,6 +73,21 @@ async def lifespan(app: FastAPI):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to create error table: {e}") from e
 
+    waiting_time_table_creation_query = """
+                CREATE TABLE IF NOT EXISTS waiting_time (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    timestamp DATETIME NOT NULL,
+                    attraction_name VARCHAR(32) NOT NULL,
+                    waiting_time INT NOT NULL,
+                    INDEX idx_waiting_time (id, timestamp, attraction_name, waiting_time)
+                );
+    """
+
+    try:
+        await database.execute(waiting_time_table_creation_query)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to create waiting_time table: {e}") from e
+
     yield
 
     try:
@@ -84,6 +104,40 @@ def authenticate_api_key(user_api_key: str = Header(None, alias=api_name)):
 # FastAPIアプリケーション起動
 app = FastAPI(lifespan=lifespan)
 
+# 営業日チェック
+def check_open_or_closed() -> str:
+    year = str(datetime.now().year)
+    month = str(datetime.now().month)
+    if  int(month) < 10:
+        month = "0" + month
+    day = str(datetime.now().day)
+
+    url = f"https://pal2.co.jp/fee/{year}/{month}/#calendar"
+    response = requests.get(url)
+    data = response.text
+
+    soup = BeautifulSoup(data, "html.parser")
+    table = soup.find("table")
+    rows = table.find_all("tr")
+
+    for row in rows:
+        cols = row.find_all("td")
+        
+        for col in cols:
+            if re.search("0", month):
+                month = month[1]
+
+            rel = col.get("rel")
+            regex = f"{month}/{day}$"
+
+            if rel is not None and re.match(regex, rel):
+                if col.find("p", class_ = "rest") is not None:
+                    return "休業日"
+                else:
+                    working_hours = col.find("a", class_ = "t_inner").get("data-time")
+                    working_hours = working_hours.replace("ï½", "~")
+                    return "営業時間: " + working_hours
+
 # APIエンドポイント
 # 入場記録
 @app.post("/api/entrance", status_code=201)
@@ -97,6 +151,9 @@ async def record_entrance(items: Entrance | list[Entrance] = Body(...), auth: bo
         items = [items]
 
     data = []
+
+    if check_open_or_closed() == "休業日":
+        return {"message": "Entrance data not recorded due to the park is closed today"}
 
     try:
         for item in items:
@@ -115,6 +172,9 @@ async def get_entrance(auth: bool = Depends(authenticate_api_key)):
     select_query = """
         SELECT COUNT(*) FROM entrance WHERE DATE(timestamp) = DATE(NOW())
     """
+
+    if check_open_or_closed() == "休業日":
+        raise HTTPException(status_code=401, detail="The park is closed today")
 
     try:
         data = await database.fetch_all(select_query)
@@ -148,12 +208,15 @@ async def get_entrance(date_from: date, date_to: date, auth: bool = Depends(auth
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to fetch data from entrance table: {e}")
 
-# 今日の入場記録の各エリアの入場数
+# 今日の各地域の入場数
 @app.get("/api/entrance/region_code", status_code=200)
 async def get_today_region_code_from_entrance(auth: bool = Depends(authenticate_api_key)):
     select_query = """
         SELECT region_code, COUNT(*) as count FROM entrance WHERE DATE(timestamp) = DATE(NOW()) GROUP BY region_code
     """
+
+    if check_open_or_closed() == "休業日":
+        raise HTTPException(status_code=401, detail="The park is closed today")
 
     try:
         region_code_list = constance.REGION_CODE_LIST        
